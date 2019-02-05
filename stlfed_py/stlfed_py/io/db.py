5d379cf032c90f26adf29c01abea0fe17df5bc90
Ryan Price <ryapric@gmail.com>
2019-02-05 15:31:28 -0600

    Clean up db_write

--- stlfed_py/stlfed_py/io/db.py
+++ stlfed_py/stlfed_py/io/db.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-def db_write(df_out, fred_id, if_exists = 'replace'):
+def db_write(df_out, fred_id, dbpath = './fcast.db', if_exists = 'replace'):
     """
     Write out `DataFrame` containing actual & forecast values to database.
     Currently writes to local SQLite DB, `fcast.db`, in working directory.
@@ -10,9 +10,11 @@ def db_write(df_out, fred_id, if_exists = 'replace'):
 
     :param fred_id: FRED series ID. Used in naming DB table.
 
+    :param dbpath: File path to SQLite DB
+
     :param if_exists: Passed to `pd.DataFrame.to_sql()`
     """
-    db = sqlite3.connect('./fcast.db')
-    df_out.to_sql(f'fcast_{fred_id}', db, index = False, if_exists = if_exists)
-    db.close()
+    con = sqlite3.connect(dbpath)
+    df_out.to_sql(f'fcast_{fred_id}', con, index = False, if_exists = if_exists)
+    con.close()
 # end db_write
